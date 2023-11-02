b996ccfd4efd89289b5bd19a3689aa096e9410e4
Bakodev <musaabdulkabeer19@gmail.com>
2023-11-02 21:25:41 +0100

    Corrected backend url

--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://blossomblendapi/.onrender.com';
+export const API_URL = 'https://blossomblendapi.onrender.com';
 // export const API_URL = 'http://localhost:9999';
 export const PAGINATE = 12;
 export const REFETCH_TIMER = 4000;
